37d5868c50ead7d41b4b1bdfcc03c12b894b035e
vitalii <vitovodenko@gmail.com>
2024-10-20 13:10:03 -0400

    Add elastic log in tosendy script

--- src/scripts/tosendy.ts
+++ src/scripts/tosendy.ts
@@ -1,6 +1,7 @@
 import { PrismaClient } from '@prisma/client';
 import axios from 'axios';
 import Bluebird from 'bluebird';
+import { Client as ElasticClient } from '@elastic/elasticsearch';
 
 import * as dotenv from 'dotenv';
 
@@ -11,6 +12,12 @@ const prisma = new PrismaClient();
 const SENDY_URL = process.env.SENDY_URL ?? 'https://sendy.blockchainmint.com';
 const SENDY_APIKEY = process.env.SENDY_APIKEY;
 const SENDY_LISTID = process.env.SENDY_LISTID;
+const ELASTIC_SEARCH_URL = process.env.ELASTIC_SEARCH_URL ?? 'http://127.0.0.1:9200';
+const ELASTIC_SENDY_LOG_INDEX = process.env.ELASTIC_SENDY_LOG ?? 'sendylog';
+
+const elastic = new ElasticClient({
+  node: ELASTIC_SEARCH_URL,
+});
 
 const addSubscribers = async () => {
   console.log('email synchronization is started');
@@ -18,21 +25,31 @@ const addSubscribers = async () => {
   await Bluebird.map(
     members,
     async (member, idx: number) => {
-      const res = await axios.post(
-        `${SENDY_URL}/subscribe`,
-        {
-          api_key: SENDY_APIKEY,
-          email: member.email,
-          name: member.fullName,
-          list: SENDY_LISTID,
-          boolean: 'true',
+      const body = {
+        api_key: SENDY_APIKEY,
+        email: member.email,
+        name: member.fullName,
+        list: SENDY_LISTID,
+        boolean: 'true',
+      };
+      const res = await axios.post(`${SENDY_URL}/subscribe`, body, {
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
         },
-        {
-          headers: {
-            'Content-Type': 'application/x-www-form-urlencoded',
-          },
-        }
-      );
+      });
+
+      elastic.index({
+        index: ELASTIC_SENDY_LOG_INDEX,
+        document: {
+          when: new Date().toISOString(),
+          api: `${SENDY_URL}/subscribe`,
+          apiMethod: 'POST',
+          body,
+          action: 'SUBSCRIBE',
+          result: res.data,
+        },
+      });
+
       console.log(`${member.username} - ${member.fullName} - ${member.email} - ${res.data}`);
     },
     { concurrency: 10 }
