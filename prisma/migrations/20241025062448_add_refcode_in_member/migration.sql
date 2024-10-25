d2fd046567762c6e684c10792cca1462747b672a
vitalii <vitovodenko@gmail.com>
2024-10-25 02:32:24 -0400

    Add refCode in member prisma

---
+++ prisma/migrations/20241025062448_add_refcode_in_member/migration.sql
@@ -0,0 +1,11 @@
+/*
+  Warnings:
+
+  - A unique constraint covering the columns `[refCode]` on the table `members` will be added. If there are existing duplicate values, this will fail.
+
+*/
+-- AlterTable
+ALTER TABLE "members" ADD COLUMN     "refCode" TEXT NOT NULL DEFAULT substring(md5(random()::text || nextval('unique_seq')::text), 1, 12);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "members_refCode_key" ON "members"("refCode");
