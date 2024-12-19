28107301916ebd225ee33f49469e62f4a9d7aaf6
vitalii <vitovodenko@gmail.com>
2024-12-19 14:52:56 -0800

    Update package service

--- src/entity/package/package.service.ts
+++ src/entity/package/package.service.ts
@@ -4,7 +4,7 @@ import { PrismaService } from '@/service/prisma';
 
 import { IDInput } from '@/graphql/common.type';
 import { CreatePackageInput, PackageQueryArgs, UpdatePackageInput } from './package.type';
-import { FREE_SHARE_ID_1, FREE_SHARE_ID_2, NO_PRODUCT } from '@/consts';
+import { NO_PRODUCT } from '@/consts';
 
 @Service()
 export class PackageService {
@@ -38,8 +38,16 @@ export class PackageService {
     });
   }
 
+  async isFreeShare(id: string) {
+    const groupSetting = await this.prisma.groupSetting.findFirst({
+      where: {
+        sponsorBonusPackageId: id,
+      },
+    });
+    return Boolean(groupSetting);
+  }
   async updatePackage(data: UpdatePackageInput) {
-    if (data.id === FREE_SHARE_ID_1 || data.id === FREE_SHARE_ID_2 || data.id === NO_PRODUCT) {
+    if (this.isFreeShare(data.id) || data.id === NO_PRODUCT) {
       throw new Error('Can not edit this product.');
     }
     const sale = await this.prisma.sale.findFirst({
