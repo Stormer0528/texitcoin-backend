80a529b6c14b611da56cb568ac3460c12523ae69
vitalii <vitovodenko@gmail.com>
2024-12-23 08:12:26 -0800

    Remove balance field in balance table prisma

---
+++ prisma/migrations/20241223161207_update_balance_table/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `balanceInCents` on the `balances` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "balances" DROP COLUMN "balanceInCents";
