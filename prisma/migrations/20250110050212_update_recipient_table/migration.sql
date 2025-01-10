0d311ca00c0f95101f3137cc75a9160a3bcab618
vitalii <vitovodenko@gmail.com>
2025-01-09 21:02:35 -0800

    Update recipient table in prisma

---
+++ prisma/migrations/20250110050212_update_recipient_table/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `isImportant` on the `recipients` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "recipients" DROP COLUMN "isImportant";
