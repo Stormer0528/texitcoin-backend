72667550412a7138286b298d4af01b4fc010f670
vitalii <vitovodenko@gmail.com>
2025-01-12 09:45:25 -0800

    Add email middleware

--- src/entity/email/email.entity.ts
+++ src/entity/email/email.entity.ts
@@ -1,10 +1,11 @@
-import { ObjectType, Field, ID } from 'type-graphql';
+import { ObjectType, Field, ID, UseMiddleware } from 'type-graphql';
 
 import { BaseEntity } from '@/graphql/baseEntity';
 
 import { Member } from '../member/member.entity';
 import { Recipient } from '../recipient/recipient.entity';
 import { PFile } from '../file/file.entity';
+import { emailAccess } from '@/graphql/middlewares';
 
 @ObjectType()
 export class Email extends BaseEntity {
@@ -32,6 +33,7 @@ export class Email extends BaseEntity {
   @Field(() => Member, { nullable: true })
   sender?: Member;
 
+  @UseMiddleware(emailAccess())
   @Field(() => [Recipient], { nullable: true })
   recipients?: Recipient[];
 
